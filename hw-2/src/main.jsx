9373c10c385e30bb3cbb7d00dd1adc71cad0bf73
pavel rubicon <virus3740@gmail.com>
2024-09-22 07:57:28 +0300

    Исправление HW2
    Исправил HW2

--- hw-2/src/main.jsx
+++ hw-2/src/main.jsx
@@ -3,38 +3,25 @@ import { restaurants } from '../database/mock';
 import './css/style.css'; 
 const root = document.getElementById("root");
 
-// ---Разбиваю данные на несколько более удобных массивов---
-let {
-  name: rName,
-  menu: rMenu,
-  reviews: rReviews
-} = restaurants;
-
-rName = restaurants.map((el) => { // Имя ресторана
-  return el.name != null && el.name != '' ? el.name : 'Имя ресторана не указано';
-});
-rMenu = restaurants.map((el) => { // Меню ресторана
-  return el.menu != null && el.menu != '' ? el.menu : 'Меню ресторана не создано';
-});
-rReviews = restaurants.map((el) => { // Отзывы ресторана
-  return el.reviews != null && el.reviews != '' ? el.reviews : 'Отзывов ещё нет';
-});
-//console.log(rName); console.log(rMenu); console.log(rReviews);
-
 createRoot(root).render(
   <div className='restorane-list'>
     {
-      rName.map((el, key) => { 
+      restaurants.map((restorane, key) => {
+				let {
+					name: restoraneName, 
+					menu: restoraneMenu,
+					reviews: restoraneReviews,
+				} = restorane;
         return ( 
         <div className='restorane-item' key={key}>
-          <div className='restorane-name'>{el}</div>
+          <div className='restorane-name'>{restoraneName}</div>
           <h3>Меню ресторана</h3>
           <ul className='restorane-catalog'>
-            {rMenu.map((el,key) => {return (<li key={key}>{el.map(item => item.name)}</li>)})}
+						{restoraneMenu.map((menu, key) => <li key={key}>{menu.name}</li>)}
           </ul>
           <h3>Отзывы</h3>
           <ul className='restorane-reviews'>
-            {rReviews.map((el,key) => {return (<li key={key}>{el.map(item => item.text)}</li>)})}
+						{restoraneReviews.map((reviews, key) => <li key={key}>{reviews.text}</li>)}
           </ul>
         </div>
         )
