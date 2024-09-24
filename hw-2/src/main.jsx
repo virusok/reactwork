4f8170f9a8baf66c493696de993494ed16da0546
pavel rubicon <virus3740@gmail.com>
2024-09-25 02:08:19 +0300

    HW2 Исправления 2
    Изменил деструктуризацию, отформатировал код

--- hw-2/src/main.jsx
+++ hw-2/src/main.jsx
@@ -5,27 +5,22 @@ const root = document.getElementById("root");
 
 createRoot(root).render(
   <div className='restorane-list'>
-    {
-      restaurants.map((restorane, key) => {
-				let {
-					name: restoraneName, 
-					menu: restoraneMenu,
-					reviews: restoraneReviews,
-				} = restorane;
-        return ( 
-        <div className='restorane-item' key={key}>
-          <div className='restorane-name'>{restoraneName}</div>
-          <h3>Меню ресторана</h3>
-          <ul className='restorane-catalog'>
-						{restoraneMenu.map((menu, key) => <li key={key}>{menu.name}</li>)}
-          </ul>
-          <h3>Отзывы</h3>
-          <ul className='restorane-reviews'>
-						{restoraneReviews.map((reviews, key) => <li key={key}>{reviews.text}</li>)}
-          </ul>
-        </div>
-        )
-      })
-    }
+  {
+    restaurants.map(({name, menu, reviews}, index) => {
+      return ( 
+      <div className='restorane-item' key={index}>
+        <div className='restorane-name'>{name}</div>
+        <h3>Меню ресторана</h3>
+        <ul className='restorane-catalog'>
+          {menu.map(menu => <li>{menu.name}</li>)}
+        </ul>
+        <h3>Отзывы</h3>
+        <ul className='restorane-reviews'>
+          {reviews.map(reviews => <li>{reviews.text}</li>)}
+        </ul>
+      </div>
+      )
+    })
+  }
   </div>
 );
