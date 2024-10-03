48c57760c23c63963cc2d712e284b18200721d43
pavel rubicon <virus3740@gmail.com>
2024-10-03 19:19:54 +0300

    Исправления ДЗ 5 + 4
    Исправления ДЗ 5 + 4

--- homework/src/template/components/restaurant/review/Review.jsx
+++ homework/src/template/components/restaurant/review/Review.jsx
@@ -1,3 +1,18 @@
-export const Review = ({ reviewItem }) => {
-	return <li>{reviewItem.text}</li>;
+import style from "./style.module.css";
+
+export const Review = ({ reviews }) => {
+	return (
+		<>
+			<h3>Отзывы</h3>
+			{reviews.length ? (
+				<ul className={style.restoraneReviews}>
+					{reviews.map((review, index) => (
+						<li key={index}>{review.text}</li>
+					))}
+				</ul>
+			) : (
+				"У ресторана нет отзывов."
+			)}
+		</>
+	);
 };
