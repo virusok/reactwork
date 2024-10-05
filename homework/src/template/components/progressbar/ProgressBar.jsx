c0ebb064e21e6a8894d8e52640d67716a19331c2
pavel rubicon <virus3740@gmail.com>
2024-10-05 18:40:25 +0300

    Исправления конфликтов при слиянии
    Исправил конфликты

--- homework/src/template/components/progressbar/ProgressBar.jsx
+++ homework/src/template/components/progressbar/ProgressBar.jsx
@@ -12,13 +12,8 @@ export const ProgressBar = () => {
 		return () => {
 			window.removeEventListener("scroll", checkHeight);
 		};
-<<<<<<< HEAD
 	}, []);
 	return (
 		<div className={style.progressbar} style={{ width: progressBarWidth }}></div>
 	);
-=======
-	}, [progressBarWidth]);
-	return <div className={progressBar} style={{ width: progressBarWidth }}></div>;
->>>>>>> main
 };
